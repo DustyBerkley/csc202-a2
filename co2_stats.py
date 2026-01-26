70dad322783d56f4d94e4e173e2a4e8decbdf2c9
Dustin Berkley <78814395+yellowww@users.noreply.github.com>
2026-01-26 12:42:37 -0800

    added data definitions

--- co2_stats.py
+++ co2_stats.py
@@ -6,17 +6,19 @@ import math
 import sys
 sys.setrecursionlimit(10**6)
 
-eh_co2_emissions : TypeAlias = Union[float, None]
-eh_co2_emisssions_per_capita : TypeAlias = Union[float, None]
-
-energy_co2_emissions: TypeAlias = Union[float, None]
-energy_co2_emissions_per_capita: TypeAlias = Union[float, None]
-
+co2_emissions : TypeAlias = Union[float, None]
+LinkedList : TypeAlias = Union['RLNode', None]
 
 @dataclass(frozen=True)
 class Row:
     country: str
     year: int
+    electricity_and_heat_co2_emissions: co2_emissions
+    electricity_and_heat_co2_emissions_per_capita: co2_emissions
+    energy_co2_emissions: co2_emissions
+    energy_co2_emissions_per_capita: co2_emissions
+    total_co2_emissions_excluding_lucf: co2_emissions
+    total_co2_emissions_excluding_lucf_per_capita: co2_emissions
     
 
 @dataclass(frozen=True)

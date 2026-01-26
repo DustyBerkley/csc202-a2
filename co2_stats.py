efa8fbc00980d36554ec2de4a7bfbf487881fb8c
cedmonst22 <cedmonst@calpoly.edu>
2026-01-26 12:36:49 -0800

    Co-authored-by: Dustin Berkley <DustyBerkley@users.noreply.github.com>

--- co2_stats.py
+++ co2_stats.py
@@ -6,8 +6,26 @@ import math
 import sys
 sys.setrecursionlimit(10**6)
 
+eh_co2_emissions : TypeAlias = Union[float, None]
+eh_co2_emisssions_per_capita : TypeAlias = Union[float, None]
+
+energy_co2_emissions: TypeAlias = Union[float, None]
+energy_co2_emissions_per_capita: TypeAlias = Union[float, None]
 
 
+@dataclass(frozen=True)
+class Row:
+    country: str
+    year: int
+    
+
+@dataclass(frozen=True)
+class RLNode:
+    first : Row
+    rest : LinkedList
+
+def read_csv_files(filename: str) -> LinkedList:
+    
 class Tests(unittest.TestCase):
     pass
 

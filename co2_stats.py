42d9ebe876aafa33f8dfdae71f177e03a3b7b571
Dustin Berkley <78814395+yellowww@users.noreply.github.com>
2026-01-26 12:20:19 -0800

    initial commit

---
+++ co2_stats.py
@@ -0,0 +1,15 @@
+import csv
+from typing import * # type: ignore
+from dataclasses import dataclass
+import unittest
+import math
+import sys
+sys.setrecursionlimit(10**6)
+
+
+
+class Tests(unittest.TestCase):
+    pass
+
+if (__name__ == '__main__'):
+    unittest.main()
